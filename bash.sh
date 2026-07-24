4cfb5ff7533d7c7a820229752a12d5a4a1d867c8
Morffino <130293159+Morffino@users.noreply.github.com>
2026-07-24 11:35:46 +0300

    Add files via upload

--- bash.sh
+++ bash.sh
@@ -1,5 +1,2 @@
-git init -b main
-git add .
-git commit -m "Ticket Bot v2.0"
-git remote add origin https://github.com/ВАШ_АККАУНТ/ticket-bot.git
-git push -u origin main
+mkdir ticket-bot
+cd ticket-bot
